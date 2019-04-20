2701780bd116a8b585deb6ced545d2e6796743c9
jinchaolu <30065376+jinchaolu@users.noreply.github.com>
2019-04-20 18:51:24 +0000

    refactor: Complete SLAM testing
    Complete SLAM testing

--- catkin_ws/test_slam.sh
+++ catkin_ws/test_slam.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 
-# TODO
 # Create a test_slam.sh shell script that launches these files
 # turtlebot_world.launch: to deploy a turtlebot in your environment
 # gmapping_demo.launch or slam_gmapping: perform SLAM
 # view_navigation.launch: observe the map in rviz
 # keyboard_teleop.launch: manually control the robot with keyboard commands
 
-# echo "Hello"
-
+# Define workspace variable
 path_catkin_ws="/home/workspace/RoboND-Term1-P5-Home-Service-Robot/catkin_ws"
-# echo "${path_catkin_ws}/src/turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch"
 
-xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch"
-: '
+# Open the workspace, source and launch turtlebot_world.launch
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm -e "${path_catkin_ws}/src/turtlebot_simulator/turtlebot_gazebo/launch/gmapping_demo.launch" &
+# Open the workspace, source and launch gmapping_demo.launch
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
-xterm -e "${path_catkin_ws}/src/turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch" &
+# Open the workspace, source and launch view_navigation.launch
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e "${path_catkin_ws}/src/turtlebot/turtlebot_teleop/launch/keyboard_teleop.launch" &
-sleep 5
-'
-
+# Open the workspace, source and launch keyboard_teleop.launch
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_teleop keyboard_teleop.launch"
