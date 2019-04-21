d15be80136212c6a6f40b10fec053e488cfe757b
jinchaolu <30065376+jinchaolu@users.noreply.github.com>
2019-04-21 23:57:37 +0000

    refactor: Complete Virtual Objects
    Complete Virtual Objects
    Add add_markers package and node
    Create add_marker.sh for demo
    Added rviz display to pick_objects.sh
    Added documentation for add_markers demo

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ Add the following line into the /home/workspace/.student_bashrc
 pip install catkin_pkg  
 pip install rospkg  
 ```
+
+catkin_create_pkg pick_objects move_base_msgs actionlib roscpp  
+catkin_create_pkg add_markers roscpp visualization_msgs  
+
+To see the marker(virtual objects) demo, in addition to running the `./add_marker.sh`, you will need to manually add a 'Marker' in rviz like the following steps:  
+Find your rviz window  
+In the left botton panel, click "Add" button  
+In 'By display type' tab, navigate the tree to 'rviz' then 'Marker'  
+Click 'OK' button  
+Done, you should see the marker(virtual objects) appear, disapper then appear again  
--- catkin_ws/add_marker.sh
+++ catkin_ws/add_marker.sh
@@ -4,7 +4,7 @@
 # turtlebot_world.launch: to deploy a turtlebot in your environment
 # amcl_demo.launch: to localize the turtlebot
 # view_navigation.launch: observe the map in rviz
-# pick_objects node: navigate to two points (pick up and drop off)
+# add_markers node: add virtual objects/markers in rviz
 
 
 # Define workspace variable
@@ -19,5 +19,5 @@ sleep 5
 # Open the workspace, source and launch view_navigation.launch
 xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-# Open the workspace, source and launch pick_objects pick_objects
-xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && rosrun pick_objects pick_objects" 
+# Open the workspace, source and launch add_markers add_markers
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && rosrun add_markers add_markers" 
